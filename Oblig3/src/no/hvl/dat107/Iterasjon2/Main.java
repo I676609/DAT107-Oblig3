5709d4af625d6afc7fc0107cdc9b626d79b8fe01
I676609 <676609@stud.hvl.no>
2024-03-14 14:01:29 +0100

    raaaahhh

--- Oblig3/src/no/hvl/dat107/Iterasjon2/Main.java
+++ Oblig3/src/no/hvl/dat107/Iterasjon2/Main.java
@@ -7,15 +7,18 @@ public class Main {
 	public static void main(String[] args) {
 		Boolean kjorer = true;
 		while (kjorer) {
+			AnsattDAO ansattDAO = new AnsattDAO();
 			String s = showInputDialog(
 					"Hva ønsker du å gjøre? \n (Finn ansatt, id, brukernavn), (Skriv ut alle ansatte), (Oppdater, Stilling, lønn), (Legg til ny ansatt)");
 			if (s.contains("Quit") || s.contains("stopp") || s.contains("Stopp")) {
 				kjorer = false;
 			}
-			else if (s.contains("Finn ansatt id")) {
-				AnsattDAO ansattDAO = new AnsattDAO();
+			else if (s.contains("Finn ansatt id") || s.contains("id") || s.contains("Id")) {
 				System.out.println(ansattDAO.finnAnsattMedId(Integer.parseInt(showInputDialog("Skriv inn ansatt id"))));
 
+			}
+			else if (s.contains("Finn ansatt brukernavn") || s.contains("Brukernavn") || s.contains("brukernavn")) {
+				System.out.println(ansattDAO.finnAnsattMedBrukernavn(showInputDialog("Skriv inn burkernavn")));
 			}
 		}
 
