d0c503f325cdf04b45e88f65b4afd7d203109fb1
h678153 <678153@stud.hvl.no>
2024-03-15 12:29:30 +0100

    123

--- Oblig3/src/no/hvl/dat107/Iterasjon3/Main.java
+++ Oblig3/src/no/hvl/dat107/Iterasjon3/Main.java
@@ -1,13 +1,21 @@
 package no.hvl.dat107.Iterasjon3;
 
-import no.hvl.dat107.Iterasjon2.Ansatt;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+import no.hvl.dat107.Iterasjon2.Ansatt;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("FirmaPU");
+		EntityManager em = emf.createEntityManager();
+		
+				
 		Ansatt bjarne = new Ansatt(2, "BjarneTheMan69", "Bjarne", "Imsdal", LocalDate.of(2015, 01, 01) , "Leder", BigDecimal.valueOf(6900000));
 		Ansatt jens = new Ansatt(3, "Jensemann", "jens", "sjokolade", LocalDate.of(2013, 02, 01) , "Utvikler", BigDecimal.valueOf(6900000));
 		Ansatt ivan = new Ansatt(4, "IvanDenGrusomme", "ivan", "bord", LocalDate.of(2017, 03, 02) , "Utvikler", BigDecimal.valueOf(6900000));
@@ -19,6 +27,8 @@ public class Main {
 		Ansatt snus = new Ansatt(10, "testing", "snusen", "snusen", LocalDate.of(2006, 10, 30) , "Stress", BigDecimal.valueOf(89));
 		Ansatt boks = new Ansatt(11, "grandis", "boks", "boksen", LocalDate.of(2015, 10, 31) , "Stress", BigDecimal.valueOf(20));
 
+		String test = "INSERT INTO Ansatt VALUES "
+		
 	}
 
 }
