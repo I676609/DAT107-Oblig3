4d4ba15dfee0b61ee2b04bd176126195103aca49
h668805 <668805@stud.hvl.no>
2024-03-15 12:20:20 +0100

    Update Main.java

--- Oblig3/src/no/hvl/dat107/Iterasjon2/Main.java
+++ Oblig3/src/no/hvl/dat107/Iterasjon2/Main.java
@@ -9,7 +9,7 @@ public class Main {
 		while (kjorer) {
 			AnsattDAO ansattDAO = new AnsattDAO();
 			String s = showInputDialog(
-					"Hva ønsker du å gjøre? \n (Finn ansatt, id, brukernavn), (Skriv ut alle ansatte), (Oppdater, Stilling, lønn), (Legg til ny ansatt)");
+					"Hva ønsker du å gjøre? \n (Finn ansatt id/brukernavn), (Skriv ut alle ansatte), (Oppdater, Stilling, lønn), (Legg til ny ansatt)");
 			if (s.contains("Quit") || s.contains("stopp") || s.contains("Stopp")) {
 				kjorer = false;
 			}
