31a74b8b919cbe3c763b36d8572de1b462010af1
h668805 <668805@stud.hvl.no>
2024-03-15 12:30:28 +0100

    Update Main.java

--- Oblig3/src/no/hvl/dat107/Iterasjon2/Main.java
+++ Oblig3/src/no/hvl/dat107/Iterasjon2/Main.java
@@ -9,19 +9,14 @@ public class Main {
 		while (kjorer) {
 			AnsattDAO ansattDAO = new AnsattDAO();
 			String s = showInputDialog(
-<<<<<<< Updated upstream
 					"Hva ønsker du å gjøre? \n Finn ansatt id/brukernavn, Skriv ut alle ansatte, Oppdater, Stilling, lønn, Legg til ny ansatt");
-=======
-					"Hva ønsker du å gjøre? \n Finn ansatt id/brukernavn, Skriv ut alle ansatte, Oppdater Stilling/lønn, Legg til ny ansatt");
->>>>>>> Stashed changes
+			
 			if (s.contains("Quit") || s.contains("stopp") || s.contains("Stopp")) {
 				kjorer = false;
-			}
-			else if (s.contains("Finn ansatt id") || s.contains("id") || s.contains("Id")) {
+			} else if (s.contains("Finn ansatt id") || s.contains("id") || s.contains("Id")) {
 				System.out.println(ansattDAO.finnAnsattMedId(Integer.parseInt(showInputDialog("Skriv inn ansatt id"))));
 
-			}
-			else if (s.contains("Finn ansatt brukernavn") || s.contains("Brukernavn") || s.contains("brukernavn")) {
+			} else if (s.contains("Finn ansatt brukernavn") || s.contains("Brukernavn") || s.contains("brukernavn")) {
 				System.out.println(ansattDAO.finnAnsattMedBrukernavn(showInputDialog("Skriv inn burkernavn")));
 			}
 		}
