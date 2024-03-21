d17cb2429e40adcb43c7c780360703c624afff52
I676609 <676609@stud.hvl.no>
2024-03-21 13:56:36 +0100

    dytt

--- Oblig3/Iterasjon3.sql
+++ Oblig3/Iterasjon3.sql
@@ -30,25 +30,25 @@ Select * from Ansatt;
 	sjef INTEGER REFERENCES Ansatt(ansattId)
 );
  ALTER TABLE Ansatt
- ADD CONSTRAINT FOREIGN KEY(avdelingID) REFERENCES Avdeling(avdelingId)
+ ADD CONSTRAINT avdelingId FOREIGN KEY(avdelingId) REFERENCES Avdeling(avdelingId);
 
 INSERT INTO
-  Ansatt(ansattId, ansettelsesDato, brukernavn, fornavn, etternavn, stilling, manedslonn)
+  Ansatt(ansettelsesDato, brukernavn, fornavn, etternavn, stilling, manedslonn)
 VALUES
-    (1, '2023-05-10', 'funnyguy87', 'Bob', 'McLaughlin', 'Chief Fun Officer', 10000.00),
-    (2, '2022-11-15', 'lolmaster', 'Chad', 'Thunderstorm', 'Director of Memes', 9000.50),
-    (3, '2023-02-28', 'quirkyqueenie', 'Alice', 'Wacky', 'Master of Puns', 8500.75),
-    (4, '2022-08-20', 'sillysally', 'Sally', 'Goofball', 'Vice President of Silliness', 12000.00),
-    (5, '2023-09-03', 'jokesterjohn', 'John', 'Whimsy', 'Minister of Laughter', 11000.25);
+    ('2023-05-10', 'funnyguy87', 'Bob', 'McLaughlin', 'Chief Fun Officer', 10000.00),
+    ('2022-11-15', 'lolmaster', 'Chad', 'Thunderstorm', 'Director of Memes', 9000.50),
+    ('2023-02-28', 'quirkyqueenie', 'Alice', 'Wacky', 'Master of Puns', 8500.75),
+    ('2022-08-20', 'sillysally', 'Sally', 'Goofball', 'Vice President of Silliness', 12000.00),
+    ('2023-09-03', 'jokesterjohn', 'John', 'Whimsy', 'Minister of Laughter', 11000.25);
 
 
-INSERT INTO avdeling(avdelingId, navn, avdelingID)
+INSERT INTO avdeling(navn, sjef)
 VALUES
-  (1, 'Innovation Hub', 1),
-  (2, 'Cybernetic Solutions', 102),
-  (3, 'Galactic Ventures', 103),
-  (4, 'FutureTech Industries', 104),
-  (5, 'Quantum Dynamics', 105);
+  ('Innovation Hub', 1),
+  ('Cybernetic Solutions', 2),
+  ('Galactic Ventures', 3),
+  ('FutureTech Industries', 4),
+  ('Quantum Dynamics', 5);
 
 
 Select * from Avdeling, Ansatt;
