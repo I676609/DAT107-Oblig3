aadb52814e7f91d536a46dfa70c1e75b0c988889
h678153 <678153@stud.hvl.no>
2024-03-21 11:39:44 +0100

    star wars

--- Oblig3/src/no/hvl/dat107/Iterasjon3/Main.java
+++ Oblig3/src/no/hvl/dat107/Iterasjon3/Main.java
@@ -15,17 +15,17 @@ public class Main {
 	}
 	
 	public void lagEksempler() {
-		Ansatt employee1 = new Ansatt(1, "jensemann", "Jens", "Jenssen", LocalDate.of(2005, 03, 05), "Leder", BigDecimal.valueOf(500000));
-		Ansatt employee2 = new Ansatt(2, "annasmith", "Anna", "Smith", LocalDate.of(2010, 10, 15), "Manager", BigDecimal.valueOf(70000));
-		Ansatt employee3 = new Ansatt(3, "mikeross", "Mike", "Ross", LocalDate.of(2008, 05, 20), "Senior Developer", BigDecimal.valueOf(90000));
-		Ansatt employee4 = new Ansatt(4, "lindawilliams", "Linda", "Williams", LocalDate.of(2015, 12, 10), "HR Coordinator", BigDecimal.valueOf(60000));
-		Ansatt employee5 = new Ansatt(5, "davidjones", "David", "Jones", LocalDate.of(2007, 08, 25), "Financial Analyst", BigDecimal.valueOf(75000));
-		Ansatt employee6 = new Ansatt(6, "emilybrown", "Emily", "Brown", LocalDate.of(2012, 04, 03), "Marketing Specialist", BigDecimal.valueOf(65000));
-		Ansatt employee7 = new Ansatt(7, "kevinwilson", "Kevin", "Wilson", LocalDate.of(2009, 11, 18), "Project Manager", BigDecimal.valueOf(85000));
-		Ansatt employee8 = new Ansatt(8, "sarahjackson", "Sarah", "Jackson", LocalDate.of(2018, 07, 07), "Software Engineer", BigDecimal.valueOf(80000));
-		Ansatt employee9 = new Ansatt(9, "chriscarter", "Chris", "Carter", LocalDate.of(2013, 09, 12), "Sales Representative", BigDecimal.valueOf(70000));
-		Ansatt employee10 = new Ansatt(10, "natalieyoung", "Natalie", "Young", LocalDate.of(2016, 02, 28), "Accountant", BigDecimal.valueOf(72000));
+		Ansatt anakinSkywalker = new Ansatt(1, "anakinskywalker", "Anakin", "Skywalker", LocalDate.of(2005, 03, 05), "The Chosen One", BigDecimal.valueOf(500000));
+		Ansatt lukeSkywalker = new Ansatt(1, "lukeskywalker", "Luke", "Skywalker", LocalDate.of(1977, 5, 25), "Jedi Knight", BigDecimal.valueOf(100000));
+		Ansatt leiaOrgana = new Ansatt(2, "leiaorgana", "Leia", "Organa", LocalDate.of(1977, 5, 25), "General", BigDecimal.valueOf(95000));
+		Ansatt hanSolo = new Ansatt(3, "hansolo", "Han", "Solo", LocalDate.of(1977, 5, 25), "Smuggler", BigDecimal.valueOf(90000));
+		Ansatt darthVader = new Ansatt(4, "darthvader", "Darth", "Vader", LocalDate.of(1977, 5, 25), "Sith Lord", BigDecimal.valueOf(120000));
+		Ansatt obiWanKenobi = new Ansatt(5, "obiwankenobi", "Obi-Wan", "Kenobi", LocalDate.of(1977, 5, 25), "Jedi Master", BigDecimal.valueOf(110000));
+		Ansatt yoda = new Ansatt(6, "yoda", "Yoda", "", LocalDate.of(1977, 5, 25), "Grand Master", BigDecimal.valueOf(150000));
+		Ansatt padmeAmidala = new Ansatt(7, "padmeamidala", "Padmé", "Amidala", LocalDate.of(1977, 5, 25), "Senator", BigDecimal.valueOf(85000));
+		Ansatt bobaFett = new Ansatt(8, "bobafett", "Boba", "Fett", LocalDate.of(1977, 5, 25), "Bounty Hunter", BigDecimal.valueOf(95000));
+		Ansatt maceWindu = new Ansatt(9, "macewindu", "Mace", "Windu", LocalDate.of(1977, 5, 25), "Jedi Master", BigDecimal.valueOf(105000));
+		Ansatt emperorPalpatine = new Ansatt(10, "emperorpalpatine", "Emperor", "Palpatine", LocalDate.of(1977, 5, 25), "Galactic Emperor", BigDecimal.valueOf(200000));
 
 	}
-
 }
