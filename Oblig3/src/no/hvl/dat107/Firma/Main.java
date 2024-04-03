92707c2ff7805827936d883348359b5787c2ed44
h668805 <668805@stud.hvl.no>
2024-04-03 11:52:39 +0200

    Update Main.java

--- Oblig3/src/no/hvl/dat107/Firma/Main.java
+++ Oblig3/src/no/hvl/dat107/Firma/Main.java
@@ -1,18 +1,18 @@
 package no.hvl.dat107.Firma;
 
-import java.math.BigDecimal;
 import static javax.swing.JOptionPane.showInputDialog;
+import java.math.BigDecimal;
 
 public class Main {
 	private static AnsattDAO ansattDAO;
 	private static AvdelingDAO avdelingDAO;
 
 	public static void main(String[] args) {
+		ansattDAO = new AnsattDAO();
+		avdelingDAO = new AvdelingDAO();
 
 		Boolean kjorer = true;
 		while (kjorer) {
-			ansattDAO = new AnsattDAO();
-			avdelingDAO = new AvdelingDAO();
 
 			String s = showInputDialog(
 					"Hva ønsker du å gjøre?\nFinn an/av (Finner en ansatt/avdeling)\nAlle (Skriver ut alle ansatte)\nOppdater (For å oppdatere en ansatt)\nLegg til (For å legge til en ny ansatt)");
@@ -62,64 +62,25 @@ public class Main {
 			if (a.toLowerCase().contains("id")) {
 				ansattDAO.oppdaterAnsattStilling(Integer.parseInt(showInputDialog("ID:")),
 						showInputDialog("Ny stilling:"));
-				System.out.println("Stilling oppdatert!!!!!!");
+				System.out.println("Stilling oppdatert!");
 			} else if (a.toLowerCase().contains("brukernavn")) {
 				ansattDAO.oppdaterAnsattStilling(showInputDialog("Brukernavn:"), showInputDialog("Ny stilling:"));
-				System.out.println("Stilling oppdatert!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+				System.out.println("Stilling oppdatert!");
 			} else
 				System.out.println("Ulivlig input!");
 		} else if (s.toLowerCase().contains("lønn")) {
 			if (a.toLowerCase().contains("id")) {
 				ansattDAO.oppdaterAnsattLonn(Integer.parseInt(showInputDialog("ID:")),
 						BigDecimal.valueOf(Double.parseDouble(showInputDialog("Ny lønn:"))));
-				System.out.println("Lønn oppdatert!!!!!!!!!!!");
-			}else if (a.toLowerCase().contains("brukernavn")) {
+				System.out.println("Lønn oppdatert!");
+			} else if (a.toLowerCase().contains("brukernavn")) {
 				ansattDAO.oppdaterAnsattLonn(showInputDialog("Brukernavn:"),
 						BigDecimal.valueOf(Double.parseDouble(showInputDialog("Ny lønn:"))));
-				System.out.println("Lønn oppdatert!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-			}else
+				System.out.println("Lønn oppdatert!");
+			} else
 				System.out.println("Ulivlig input!");
 		} else {
 			System.out.println("Ulovlig input!");
 		}
-
-//	public static void lagEksempler() {
-//		Avdeling tattoine = new Avdeling("Tattoine");
-//		Avdeling endor = new Avdeling("Endor");
-//		Avdeling hoth = new Avdeling("hoth");
-
-//		Ansatt anakinSkywalker = new Ansatt("anakinskywalker", "Anakin", "Skywalker", LocalDate.of(2005, 03, 05),
-//				"The Chosen One", BigDecimal.valueOf(500000), tattoine);
-//
-//		Ansatt lukeSkywalker = new Ansatt("lukeskywalker", "Luke", "Skywalker", LocalDate.of(1977, 5, 25),
-//				"Jedi Knight", BigDecimal.valueOf(100000), endor);
-//
-//		Ansatt leiaOrgana = new Ansatt("leiaorgana", "Leia", "Organa", LocalDate.of(1977, 5, 25), "General",
-//				BigDecimal.valueOf(95000), hoth);
-//
-//		Ansatt hanSolo = new Ansatt("hansolo", "Han", "Solo", LocalDate.of(1977, 5, 25), "Smuggler",
-//				BigDecimal.valueOf(90000), hoth);
-//
-//		Ansatt darthVader = new Ansatt("darthvader", "Darth", "Vader", LocalDate.of(1977, 5, 25), "Sith Lord",
-//				BigDecimal.valueOf(120000), tattoine);
-//
-//		Ansatt obiWanKenobi = new Ansatt("obiwankenobi", "Obi-Wan", "Kenobi", LocalDate.of(1977, 5, 25), "Jedi Master",
-//				BigDecimal.valueOf(110000), endor);
-//
-//		Ansatt yoda = new Ansatt("yoda", "Yoda", "", LocalDate.of(1977, 5, 25), "Grand Master",
-//				BigDecimal.valueOf(150000), endor);
-//
-//		Ansatt padmeAmidala = new Ansatt("padmeamidala", "Padmé", "Amidala", LocalDate.of(1977, 5, 25), "Senator",
-//				BigDecimal.valueOf(85000), hoth);
-//
-//		Ansatt bobaFett = new Ansatt("bobafett", "Boba", "Fett", LocalDate.of(1977, 5, 25), "Bounty Hunter",
-//				BigDecimal.valueOf(95000), tattoine);
-//
-//		Ansatt maceWindu = new Ansatt("macewindu", "Mace", "Windu", LocalDate.of(1977, 5, 25), "Jedi Master",
-//				BigDecimal.valueOf(105000), endor);
-//
-//		Ansatt emperorPalpatine = new Ansatt("emperorpalpatine", "Emperor", "Palpatine", LocalDate.of(1977, 5, 25),
-//				"Galactic Emperor", BigDecimal.valueOf(200000), hoth);
-
 	}
 }
